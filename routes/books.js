419f88ca73698e59c776e3744d539f3bc4545a4f
davidtan <davidtan@thoughtworks.com>
2018-03-15 10:23:25 +0800

    Implement CRUD routes for /books

--- routes/books.js
+++ routes/books.js
@@ -2,8 +2,24 @@ var express = require("express");
 var router = express.Router();
 
 /* GET books listing. */
-router.get("/", function(req, res, next) {
-  res.json("respond with all books");
+router.get("/", function(req, res) {
+  res.json({ message: "respond with all books" });
+});
+
+router.get("/:id", function(req, res) {
+  res.json({ message: `get book with id ${req.params.id}` });
+});
+
+router.post("/", function(req, res) {
+  res.json({ message: `create new book using data from ${req.body}` });
+});
+
+router.put("/:id", function(req, res) {
+  res.json({ message: `update book with id ${req.params.id}` });
+});
+
+router.delete("/:id", function(req, res) {
+  res.json({ message: `delete book with id ${req.params.id}` });
 });
 
 module.exports = router;
