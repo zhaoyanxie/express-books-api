4a66a916c320a7362debb181f2d6568d099c3e84
davidtan <davidtan@thoughtworks.com>
2018-03-16 14:43:17 +0800

    Refactor ES5 to ES6 syntax

--- routes/books.js
+++ routes/books.js
@@ -1,24 +1,24 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
 /* GET books listing. */
-router.get("/", function(req, res) {
+router.get("/", (req, res, next) => {
   res.json({ message: "respond with all books" });
 });
 
-router.get("/:id", function(req, res) {
+router.get("/:id", (req, res, next) => {
   res.json({ message: `get book with id ${req.params.id}` });
 });
 
-router.post("/", function(req, res) {
+router.post("/", (req, res, next) => {
   res.json({ message: `create new book using data from ${req.body}` });
 });
 
-router.put("/:id", function(req, res) {
+router.put("/:id", (req, res, next) => {
   res.json({ message: `update book with id ${req.params.id}` });
 });
 
-router.delete("/:id", function(req, res) {
+router.delete("/:id", (req, res, next) => {
   res.json({ message: `delete book with id ${req.params.id}` });
 });
 
