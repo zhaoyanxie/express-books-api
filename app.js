0e1771670ed9aa32d109f05d4498580af602b9b5
davidtan <davidtan@thoughtworks.com>
2018-03-16 16:22:14 +0800

    Remove unused dependency

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const favicon = require("serve-favicon");
 const logger = require("morgan");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
 
 const index = require("./routes/index");
 const books = require("./routes/books");
